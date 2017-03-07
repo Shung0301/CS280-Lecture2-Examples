5ed9fe64dbae60f3634122a3a7b829cf36075685
Shung0301 <tylertangbu@gmail.com>
2017-03-07 20:29:48 +0800

    新增round-off errors範例

---
+++ Example01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Console.Write("使用者名稱:");
+            //string userName = Console.ReadLine();
+            //Console.Write("請輸入密碼");
+            //string userPassword = Console.ReadLine();
+            //Console.WriteLine("{0} 您好，登入成功", userName);
+
+            Console.WriteLine(0.1f + 0.2f == 0.3f);
+            Console.WriteLine(1.0f + 0.33f == 1.33f);
+            Console.WriteLine(1 / 2 * 10);
+            Console.WriteLine(1f / 2f * 10);
+        }
+    }
+}
